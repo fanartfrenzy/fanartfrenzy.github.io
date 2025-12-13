206dc8216910506e88c968dd1b21094fcd77f12c
Clara Martens Ávila <clara@nextnovate.com>
2025-12-13 19:21:15 +0100

    added all the guide pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,10 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     $("#mainbody").load("pages/home.html");
+    $("#navlist").load("pages/elements/nav.html");
+    $("#footertext").load("pages/elements/footertext.html");
 });
-document.getElementById("navlist").innerHTML = `
-<ul class="">
-    <li><a onclick='open_Home()'>Home</a></li>
-    <li><a onclick='open_FAQ()'>FAQ</a></li>
-    <li><a onclick='open_Tutorials()'>Tutorials</a></li>
-    <li><a onclick='open_About()'>About</a></li>
-    <li><a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfi7LfeJM7om5ewgvcLPC8v9YokE1NHcWbgqPttzR6SAzywrg/viewform"><mark class="signupbtn">Sign up</mark></a></li>
-</ul>
-`;
+
 
 function open_Tutorials() {
   $("#mainbody").load("pages/tutorials.html", function() {
@@ -25,10 +19,6 @@ function open_Tutorials() {
 
 function open_Home() {
     $("#mainbody").load("pages/home.html");
-}
-
-function open_About() {
-    $("#mainbody").load("pages/about.html");
 }
 
 function open_FAQ() {
@@ -56,6 +46,23 @@ function open_FAQ() {
   });
 }
 
-document.getElementById("footertext").innerHTML = "New Content";
+
+// ABOUT PAGES
+function open_About() {
+    $("#mainbody").load("pages/about.html");
+}
+
+function open_Rules() {
+    $("#mainbody").load("pages/subpages_about/rules.html");
+}
+
+function open_Commenting() {
+    $("#mainbody").load("pages/subpages_about/commenting_guide.html");
+}
+
+function open_Posting() {
+    $("#mainbody").load("pages/subpages_about/posting_guide.html");
+}
+
 
 
