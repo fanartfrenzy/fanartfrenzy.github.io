38da41d16adb44d5fee06c16cc1a0a421483c21b
Clara Martens Ávila <clara@nextnovate.com>
2025-12-14 17:08:32 +0100

    removed http urls

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,50 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+$(document).ready(function() {
+
+    // Handles # in urls
+    function loadPage() {
+        // 1. Get the hash (e.g., "#about"), remove the '#'
+        // If the hash is empty (the user just visited site.com), default to 'home'
+        var hash = window.location.hash.substring(1);
+
+        // 2. Load the content
+        // This assumes you have files named home.html, about.html, etc.
+        console.log(hash)
+        if (hash == 'commenting') {
+            open_Commenting(); 
+        } else if (hash == 'rules') {
+            open_Rules();
+        } else if (hash == 'posting') {
+            open_Posting();
+        } else if (hash == 'about') {
+            open_About();
+        } else if (hash == 'tutorials') {
+            open_Tutorials();
+        } else if (hash == 'faq') {
+            open_FAQ();
+        } else {
+            open_Home();
+        }
+        // $('#main-content').load(hash + '.html', function(response, status, xhr) {
+        //     if (status == "error") {
+        //         $('#main-content').html("<h1>Sorry, page not found.</h1>");
+        //     }
+        // });
+    }
+
+    // 3. Listen for hash changes (User clicks link or hits back button)
+    $(window).on('hashchange', loadPage);
+
+    // 4. Trigger the function immediately on page load
+    // This ensures if someone refreshes "site.com/#about", they stay on About.
+    loadPage();
+
+});
+
+
+
 function open_Tutorials() {
   $("#mainbody").load("pages/tutorials.html", function() {
     var elems = document.querySelectorAll('#mainbody .collapsible'); // Scoped selector
